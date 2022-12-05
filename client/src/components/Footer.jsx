fa35d96f2312285f4e691ac853644276f486b8b9
Paula Speciale <paulaspeciale@gmail.com>
2022-12-05 18:41:56 -0300

    agregando los iconos

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <div>
+     <div><img alt="aca va la img del perrito"/></div>
+     <div>
+     <p>+54 9 11 2537 1316</p>
+     <p>versiagregamosmail@mail.com.ar</p>
+     <p>Estamos en Esteban Echeverria, Argentina.</p>
+     </div>
+     <div>
+      <p>¡Seguinos!</p>
+      {/*aca van los logos de las redes sociales y los links */}
+     </div>
+     <p>El Campito Refugio Todos los derechos reservados.©</p>
+    </div>
+  );
+};
+
+export default Footer;
